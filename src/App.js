fc293ddafb50be8d4e8fe797f086c9c1adcb28fa
Andrea Menendez <andreaveronicam96@gmail.com>
2023-03-10 17:59:59 -0300

    Cree el Navbar y paginas para navegar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import NavBar from './components/navBar/Navbar'
+import{Routes,Route} from 'react-router-dom'
+import About from './components/pages/About';
+import Home from './components/pages/Home';
+import Error from './components/pages/Error';
 
 function App() {
   return (
     <div className="App">
-
+      <NavBar></NavBar>
+        <Routes>
+          <Route path='/home' element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path="*" element={<Error/>}/>
+        </Routes>
     </div>
   );
 }
